73bcf0156175ddfa0b6d72bbdc944177079b5115
Nasir Islam Sujan <islam35-1102@diu.edu.bd>
2019-08-19 15:10:47 -0700

    adding bunch of new codes

--- App_Start/WebApiConfig.cs
+++ App_Start/WebApiConfig.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Newtonsoft.Json.Serialization;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web.Http;
@@ -7,6 +8,7 @@ namespace WebAPI
 {
     public static class WebApiConfig
     {
+        public class CustomJson { }
         public static void Register(HttpConfiguration config)
         {
             // Web API configuration and services
@@ -19,6 +21,21 @@ namespace WebAPI
                 routeTemplate: "api/{controller}/{id}",
                 defaults: new { id = RouteParameter.Optional }
             );
+
+
+            #region remove specific data formatters
+            // remove xml formatter
+            config.Formatters.Remove(config.Formatters.XmlFormatter);
+            #endregion
+
+            #region make JSON indented
+            //config.Formatters.JsonFormatter.SerializerSettings.Formatting = Newtonsoft.Json.Formatting.Indented;
+            #endregion
+
+            #region for camelcase property
+            //config.Formatters.JsonFormatter.SerializerSettings.ContractResolver = new CamelCasePropertyNamesContractResolver();
+            #endregion
+
         }
     }
 }
