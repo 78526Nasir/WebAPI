77a53ddf4891223f6cc024887e84bc094254890e
Nasir Islam Sujan <islam35-1102@diu.edu.bd>
2019-08-19 18:21:41 -0700

    Calling ASP.NET api from code behind

---
+++ Pages/EmployeeDashborad.aspx
@@ -0,0 +1,33 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="EmployeeDashborad.aspx.cs" Inherits="WebAPI.Pages.EmployeeDashborad" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+    <style>
+        table{
+            border:1px solid #cecece;
+        }
+        table tr:nth-child(even){
+            background:#f5f5f5;
+        }
+        table td, table th{
+            padding:7px 12px;
+        }
+        table th{
+            background:#ddd;
+        }
+        table tr:nth-child(odd):hover{
+            background:#f5f5f5;
+        }
+    </style>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div>
+            <asp:GridView ID="gv1" runat="server"></asp:GridView>
+        </div>
+    </form>
+</body>
+</html>
---
+++ Pages/EmployeeDashborad.aspx.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using WebAPI.Models;
+
+namespace WebAPI.Pages
+{
+    public partial class EmployeeDashborad : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            LoadGridView();
+        }
+
+        private void LoadGridView()
+        {
+            IEnumerable<Employee> employees = null;
+            using (var client = new HttpClient())
+            {
+                client.BaseAddress = new Uri("http://localhost:55656");
+                client.DefaultRequestHeaders.Accept.Clear();
+                client.DefaultRequestHeaders.Accept.Add(new System.Net.Http.Headers.MediaTypeWithQualityHeaderValue("application/json"));
+                var response = client.GetAsync("/api/employees").Result;
+
+                if (response.IsSuccessStatusCode)
+                {
+                    string responseString = response.Content.ReadAsStringAsync().Result;
+                    Newtonsoft.Json.Linq.JArray json = Newtonsoft.Json.Linq.JArray.Parse(responseString);
+                    employees = Newtonsoft.Json.JsonConvert.DeserializeObject<IEnumerable<Employee>>(responseString);
+                }
+
+                gv1.DataSource = employees;
+                gv1.DataBind();
+            }
+        }
+    }
+}
